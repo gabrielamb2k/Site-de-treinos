9e5fe017b9b8954f35cd37ddd01393cc824bf01f
gabrielamb2k <gambomfim9@gmail.com>
2025-08-12 20:01:00 -0300

    primeiro commit

---
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/GeradorDeTreinosApplication.java
@@ -0,0 +1,13 @@
+package aplicativo.treino.Gerador.de.treinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeTreinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeTreinosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aplicativo/treino/Gerador/de/treinos/GeradorDeTreinosApplicationTests.java
@@ -0,0 +1,13 @@
+package aplicativo.treino.Gerador.de.treinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDeTreinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
