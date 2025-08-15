80965b3b6522efb14fc48fa479df0a56f6a4eccc
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-15 12:48:13 -0300

    criando exercise Exercise.java
    falta completar get/set

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/Exercise.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/Exercise.java
@@ -1,4 +1,29 @@
 package aplicativo.treino.Gerador.de.treinos.domain;
 
 public abstract class Exercise {
+   /*Atributos*/
+  String nome;
+  int reps;
+  int series;
+  String obs;
+   /*builder*/
+  public Exercise(String nome, int reps, int series, String obs){
+    this.nome=nome;
+    this.reps=reps;
+    this.series=series;
+    this.obs=obs;
+  }
+  /*get/set*/
+  public String getNome(){
+    return nome;
+  }
+  public void setNome(String nome){
+    this.nome=nome;
+  }
+  public int getReps(){
+    return reps;
+  }
+  public void setReps(int reps){
+    this.reps=reps;
+  }
 }
