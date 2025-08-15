964b344c40ef8a0cf3295399fb7ece9c0d5bce2c
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-15 12:08:07 -0300

    Criação do WorkoutTemplate.java
    por enquanto não criei o atributo " int dias "

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/WorkoutTemplate.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/WorkoutTemplate.java
@@ -1,4 +1,32 @@
 package aplicativo.treino.Gerador.de.treinos.domain;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class WorkoutTemplate {
+    private List<Exercise> exercises;
+    //div == divisão de treinos (ex: upper/lower, fullbody etc... lembrando que a div define a quantidade de dias à ir na academia)
+    private String div;
+    
+
+    public WorkoutTemplate(String div){
+      this.div=div;
+        this.exercises= new ArrayList<>();
+    }
+
+    public String getDiv() {
+        return div;
+    }
+
+    public void setDiv(String div) {
+        this.div = div;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
 }
