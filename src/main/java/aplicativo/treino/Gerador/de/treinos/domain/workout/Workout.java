da1453630a36faf364190223acf8c0daf89d1327
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 09:13:48 -0300

    Update Workout.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/Workout.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/Workout.java
@@ -1,4 +1,8 @@
 package aplicativo.treino.Gerador.de.treinos.domain.workout;
+import aplicativo.treino.Gerador.de.treinos.domain.exercise.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Workout extends WorkoutTemplate {
   public Workout(String div, int duracao){
