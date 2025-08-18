198875f8eadd8916c9f78c96dc577990f5212318
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 13:16:02 -0300

    Update GymWorkoutService.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/service/GymWorkoutService.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/service/GymWorkoutService.java
@@ -1,4 +1,31 @@
 package aplicativo.treino.Gerador.de.treinos.service;
 
+import aplicativo.treino.Gerador.de.treinos.domain.workout.GymWorkout;
+import aplicativo.treino.Gerador.de.treinos.repository.GymWorkoutRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Classe de serviço da {@link HybridWorkout}
+ * @version 1.0
+ */
+@Service
 public class GymWorkoutService {
+
+    private GymWorkoutRepository gymWorkoutRepository;
+
+    public GymWorkoutService(GymWorkoutRepository gymWorkoutRepository){
+        this.gymWorkoutRepository=gymWorkoutRepository;
+    }
+
+    public void save(GymWorkout gymWorkout){
+        gymWorkoutRepository.save(gymWorkout);
+    }
+
+    public List<GymWorkout> gymWorkoutList(){
+        return gymWorkoutRepository.list();
+    }
+
 }
+
