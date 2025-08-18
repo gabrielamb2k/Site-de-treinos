0f52dd41e76a89fba2052d0a1871b20a989c136a
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 09:09:21 -0300

    extends GymExercice.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
@@ -1,6 +1,6 @@
 package aplicativo.treino.Gerador.de.treinos.domain.exercise;
 
-public class GymExercice {
+public class GymExercice extends Exercise{
    public GymExercice(String nome, int reps, int series, String obs) {
         super(nome, reps, series, obs);
     }
