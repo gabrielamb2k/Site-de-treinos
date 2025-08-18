74a0ce43452220de17d7c5abfa5a28f9ac2679a8
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 09:10:54 -0300

    comentando GymExercice.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
@@ -1,5 +1,6 @@
 package aplicativo.treino.Gerador.de.treinos.domain.exercise;
-
+/* Classe para exercicios de academia, basicamente segue o padrão de exercise, sem aérobico
+*/
 public class GymExercice extends Exercise{
    public GymExercice(String nome, int reps, int series, String obs) {
         super(nome, reps, series, obs);
