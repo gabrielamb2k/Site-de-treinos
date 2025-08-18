953b9d4e21194f1fe57d15e2704a26922adb7327
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 09:08:59 -0300

    criando GymExercice.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
@@ -1,4 +1,7 @@
 package aplicativo.treino.Gerador.de.treinos.domain.exercise;
 
 public class GymExercice {
+   public GymExercice(String nome, int reps, int series, String obs) {
+        super(nome, reps, series, obs);
+    }
 }
