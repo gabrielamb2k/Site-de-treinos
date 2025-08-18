ca9c8db064b2cd949ee512d22b019da8e48a49e5
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 09:31:31 -0300

    add o diferencial de aquecimento GymExercice.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/exercise/GymExercice.java
@@ -1,8 +1,20 @@
 package aplicativo.treino.Gerador.de.treinos.domain.exercise;
-/* Classe para exercicios de academia, basicamente segue o padrão de exercise, sem aérobico
+/* Classe para exercicios de academia, basicamente segue o padrão de exercise,add apenas o boolean isAquec para identificar se o exercicio
+é de aquecimento ou não
 */
 public class GymExercice extends Exercise{
-   public GymExercice(String nome, int reps, int series, String obs) {
+    private boolean isAquec; // indica se é exercício de aquecimento
+
+   public GymExercice(String nome, int reps, int series, String obs,boolean isAquec) {
         super(nome, reps, series, obs);
+        this.isAquec = isAquec;
+    }
+
+    public boolean isAquec() {
+        return isAquec;
+    }
+
+    public void setAquec(boolean aquec) {
+        isAquecimento = aquec;
     }
 }
