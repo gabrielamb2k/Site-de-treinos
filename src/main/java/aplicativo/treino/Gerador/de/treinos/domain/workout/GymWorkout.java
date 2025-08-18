0a0a96c6eabb25bc940a3596ac0e5a647c549425
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 12:42:13 -0300

    Update GymWorkout.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/GymWorkout.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/GymWorkout.java
@@ -3,7 +3,7 @@ import aplicativo.treino.Gerador.de.treinos.domain.exercise.GymExercice;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 public class GymWorkout extends Workout{
   //lista de exercicios para academia(puxar ferro)
