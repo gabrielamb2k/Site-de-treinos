589fc366ce0030201f4ea7298bd7ce5b891ba92d
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-18 13:07:38 -0300

    Update GymWorkoutRepository.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/repository/GymWorkoutRepository.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/repository/GymWorkoutRepository.java
@@ -1,4 +1,10 @@
 package aplicativo.treino.Gerador.de.treinos.repository;
+import aplicativo.treino.Gerador.de.treinos.domain.workout.GymWorkout;
+
+import java.util.List;
 
 public interface GymWorkoutRepository {
+    void save(GymWorkout gymWorkout);
+    List<GymWorkout> list();
+
 }
