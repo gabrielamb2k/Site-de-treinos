719f594120d4fcab9a09ba9a72ee13b03852eade
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-27 11:16:41 -0300

    aplicando o dataGer direto em Workout.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/Workout.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/Workout.java
@@ -5,7 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 //Herda de WorkoutTemplate
 public abstract class Workout extends WorkoutTemplate {
-  public Workout(String div, int duracao,LocalDateTime dataGer){
-    super(div,duracao,dataGer);
+  //dataGer== data que o treino foi gerado, implementado para ajudar na criação do historico
+    private LocalDateTime dataGer;
+  
+  public Workout(String div, int duracao){
+    super(div,duracao);
+    this.dataGer=LocalDateTime.now();
   }
+  //somente o get pois o dataGer por padrão será setado por: LocalDateTime.now()
+     public LocalDateTime getData() {
+        return dataGer;
+    }
 }
