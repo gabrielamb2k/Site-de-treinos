11ccf89836dc51c50b8d0d75e1f85dcc090d9d70
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-27 14:40:01 -0300

    Update WorkoutTemplate.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/WorkoutTemplate.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/WorkoutTemplate.java
@@ -1,6 +1,6 @@
 package aplicativo.treino.Gerador.de.treinos.domain.workout;
 import aplicativo.treino.Gerador.de.treinos.domain.exercise.Exercise;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
