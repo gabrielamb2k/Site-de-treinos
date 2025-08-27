8be21ba2a5b7fa94d6cf8c159921bf1e8cebf9eb
yanrsa-dev <yanrochadesa10@gmail.com>
2025-08-27 14:35:36 -0300

    Update Workout.java

--- src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/Workout.java
+++ src/main/java/aplicativo/treino/Gerador/de/treinos/domain/workout/Workout.java
@@ -1,9 +1,8 @@
 package aplicativo.treino.Gerador.de.treinos.domain.workout;
+import java.time.LocalDate;
 import aplicativo.treino.Gerador.de.treinos.domain.exercise.Exercise;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDate;
 //Herda de WorkoutTemplate
 public abstract class Workout extends WorkoutTemplate {
   //dataGer== data que o treino foi gerado, implementado para ajudar na criação do historico
